c562e9482566f1d56ccad044fb0c8b3b41ea053a
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2021-08-03 23:44:14 +0800

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,17 @@
+export default {
+  components: true,
+  head: {
+    titleTemplate: 'Mastering Nuxt: %s',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      {
+        charset: 'utf-8'
+      }
+    ],
+    bodyAttrs: {
+      class: ['my-style']
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div v-for="home in homes" :key="home.objectID" style="float: left; margin: 10px">
+      <HomeCard :home="home" />
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from '~/data/homes';
+
+export default {
+  head(){
+    // adding head tags
+    return {
+      title: 'Homepage',
+      meta: [
+        {
+          name: 'description',
+          content: 'This is the homepage description',
+          hid: 'description'
+        }
+      ]
+    }
+  },
+  data(){
+    return {
+      homes: homes.slice(0, 3)
+    }
+  }
+}
+</script>
