505a0181d69f383e2ac382885164e9672c7469c5
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2021-08-23 08:39:46 +0800

    Add Fetch homes by location using Google Autocomplete

--- pages/search.vue
+++ pages/search.vue
@@ -1,5 +1,35 @@
 <template>
   <div>
-    {{ $route.query.lat }} {{ $route.query.lng }}{{ $route.query.label }}
+    <div v-for="home in homes" :key="home.objectID">
+      {{ home.title }}
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  async beforeRouteUpdate(to, from, next) {
+    const response = await this.dataApi.getHomesByLocation(
+      to.query.lat,
+      to.query.lng,
+    );
+
+    this.homes = response.data.hits;
+    this.label = to.query.label;
+    this.lat = to.query.lat;
+    this.lng = to.query.lng;
+
+    next();
+  },
+  async asyncData({ query, $dataApi }) {
+    const response = await $dataApi.getHomesByLocation(query.lat, query.lng);
+
+    return {
+      homes: response.data.hits,
+      label: query.label,
+      lat: query.lat,
+      lng: query.lng,
+    };
+  },
+};
+</script>
--- plugins/dataApi.js
+++ plugins/dataApi.js
@@ -94,11 +94,34 @@ export default function (context, inject) {
     }
   }
 
+  async function getHomesByLocation(lat, lng, radiusInMeters = 1500) {
+    try {
+      const response = await fetch(
+        `https://${process.env.agoliaAppId}-dsn.algolia.net/1/indexes/homes/query`,
+        {
+          headers,
+          method: 'POST',
+          body: JSON.stringify({
+            aroundLatLng: `${lat},${lng}`,
+            aroundRadius: radiusInMeters,
+            hitsPerPage: 10,
+            attributesToHighlight: [], // remove _highlightResult property
+          }),
+        },
+      );
+
+      return unwrap(response);
+    } catch (error) {
+      return getErrorResponse(error);
+    }
+  }
+
   // inject dataApi into Nuxt context
   inject('dataApi', {
     getHome,
     getHomes,
     getReviewsByHomeId,
     getUsersByHomeId,
+    getHomesByLocation,
   });
 }
