37c7ee3ace1f13575c4877076e2d9b6e2c432287
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2021-08-24 08:35:16 +0800

    Show GoogleMaps location upon searching

--- pages/search.vue
+++ pages/search.vue
@@ -1,12 +1,17 @@
 <template>
-  <div v-if="homes.length > 0">
-    <HomeRow v-for="home in homes" :key="home.objectID" :home="home" />
+  <div>
+    <h4>Results for {{ label }}</h4>
+    <div ref="map" style="width: 800px; height: 800px; float: right"></div>
+    <div v-if="homes.length > 0">
+      <HomeRow v-for="home in homes" :key="home.objectID" :home="home" />
+    </div>
+    <div v-else>No Results Found</div>
   </div>
-  <div v-else>No Results Found</div>
 </template>
 
 <script>
 export default {
+  // runs whenever route changes (query params changes)
   async beforeRouteUpdate(to, from, next) {
     const response = await this.$dataApi.getHomesByLocation(
       to.query.lat,
@@ -18,8 +23,12 @@ export default {
     this.lat = to.query.lat;
     this.lng = to.query.lng;
 
+    // update map whenever query params changes
+    this.updateMap();
+
     next();
   },
+
   async asyncData({ query, $dataApi }) {
     const response = await $dataApi.getHomesByLocation(query.lat, query.lng);
 
@@ -30,10 +39,22 @@ export default {
       lng: query.lng,
     };
   },
+
   head() {
     return {
       title: `Homes around ${this.label}`,
     };
+  },
+
+  mounted() {
+    // show the map when the page is mounted
+    this.updateMap();
+  },
+
+  methods: {
+    updateMap() {
+      this.$maps.showMap(this.$refs.map, this.lat, this.lng);
+    },
   },
 };
 </script>
