0fd20662cade94a5929496c694d1ee90a36ef6d4
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2021-09-04 15:52:58 +0800

    Styling layout header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,28 @@
 <template>
-  <div>
+  <div class="app">
+    <header class="app-header">
+      <div class="app-logo">
+        <img src="/images/logo.svg" />
+      </div>
+      <div class="app-search">
+        <input
+          ref="inputRef"
+          type="text"
+          placeholder="Enter your address"
+          @changed="go"
+        />
+        <input type="text" class="datepicker" placeholder="Check in" />
+        <input type="text" class="datepicker" placeholder="Check out" />
+        <button>
+          <img src="/images/icons/search.svg" />
+        </button>
+      </div>
+      <div class="app-user-menu">
+        <img src="/images/icons/house.svg" />
+        <div class="name">Host</div>
+        <img src="/images/user.jpg" class="avatar" />
+      </div>
+    </header>
     <header>
       <NuxtLink to="/">Home</NuxtLink>
       <input ref="inputRef" type="text" @changed="go" />
